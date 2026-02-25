c6b62aa4ac2a4ab45fc17e330435c13fefc1ddc1
fernandesronal111-ctrl <fernandesronal111@gmail.com>
2026-02-25 16:49:35 +0530

    railway db added

--- search/connect.php
+++ search/connect.php
@@ -1,10 +1,11 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "fish_data1";
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$db   = getenv("DB_NAME");
+$port = getenv("DB_PORT") ?: 3306;
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
